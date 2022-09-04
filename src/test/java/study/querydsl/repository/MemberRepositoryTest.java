6cf374f09236c127c5b680bd55d2559b3c6153c3
Yongje Shin <yongje1211@gmail.com>
2022-09-04 21:28:35 +0900

    2022-09-04 Spring Data JPA Querydsl 기능 1. QuerydslPredicateExecutor 2. Querydsl4RepositorySupport 추가

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -97,4 +97,31 @@ class MemberRepositoryTest {
         assertThat(result).extracting("username").containsExactly("member1", "member2", "member3");
     }
 
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40)
+                        .and(member.username.eq("member1")));
+
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
+    }
 }
